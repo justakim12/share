5eef4ce3a3e3d5a292ef2da11e0af17315072e01
kyungil-david <kyungil.david@navercorp.com>
2022-08-20 12:15:33 +0900

    Update to version3

--- refactoring_220812/check_complexity/define_properties.py
+++ refactoring_220812/check_complexity/define_properties.py
@@ -1,29 +1,39 @@
 def define_properties(region, env):
-    if region == "kr":
-        if env == "qa":
-            region_timezone = "KST"
-            city_name = "Seoul"
-            threshold = 10
-        elif env == "prod":
-            region_timezone = "KST"
-            city_name = "Busan"
-            threshold = 10
-    elif region == "jp":
-        if env == "qa":
-            region_timezone = "JST"
-            city_name = "Tokyo"
-            threshold = 40
-        elif env == "prod":
-            region_timezone = "JST"
-            city_name = "Tokyo"
-            threshold = 20
-    elif region == "usa":
-        if env == "qa":
-            region_timezone = "PST"
-            city_name = "San Francisco"
-            threshold = 20
-        elif env == "prod":
-            region_timezone = "PST"
-            city_name = "San Francisco"
-            threshold = 20
+    properties = {
+        "kr":
+            {"qa":
+                 {"region_timezone": "KST",
+                  "city_name": "Seoul",
+                  "threshold": 10},
+             "prod":
+                 {"region_timezone": "KST",
+                  "city_name": "Seoul",
+                  "threshold": 10},
+             },
+        "jp":
+            {"qa":
+                 {"region_timezone": "JST",
+                  "city_name": "Tokyo",
+                  "threshold": 40},
+             "prod":
+                 {"region_timezone": "JST",
+                  "city_name": "Tokyo",
+                  "threshold": 20},
+             },
+        "usa":
+            {"qa":
+                 {"region_timezone": "PST",
+                  "city_name": "San Francisco",
+                  "threshold": 20},
+             "prod":
+                 {"region_timezone": "PST",
+                  "city_name": "San Francisco",
+                  "threshold": 20},
+             },
+    }
+
+    region_timezone = properties[region][env]["region_timezone"]
+    city_name = properties[region][env]["city_name"]
+    threshold = properties[region][env]["threshold"]
+
     return region_timezone, city_name, threshold
--- refactoring_220812/check_complexity/main.py
+++ refactoring_220812/check_complexity/main.py
@@ -1,7 +1,8 @@
 from refactoring_220812.version2.define_properties import define_properties
 
 
-# Split function to separate logic. Lowered code complexity.
+# Changed the repeated conditional statement into a dictionary format
+# to further lower code complexity.
 def activate(region, env):
     region_timezone, city_name, threshold = define_properties(region, env)
     threshold = threshold + 10
